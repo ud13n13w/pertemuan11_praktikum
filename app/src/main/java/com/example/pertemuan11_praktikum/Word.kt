3e622451223f8828a35d12ae9a3f054d191bda21
ud13n13w <muhammadburhanuddinfawwaz@gmail.com>
2020-12-01 12:31:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan11_praktikum/Word.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan11_praktikum
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+
---
+++ app/src/main/java/com/example/pertemuan11_praktikum/WordRoomDatabase.kt
@@ -0,0 +1,66 @@
+package com.example.pertemuan11_praktikum
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Database (entities = arrayOf(Word::class), version = 1, exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase(){
+
+    abstract fun wordDao(): WordDao
+
+    private class WordDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteALL()
+
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+            // TODO: Add your own words!
+        }
+
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context,scope: CoroutineScope): WordRoomDatabase {
+            val tempInstance = INSTANCE
+
+            if (tempInstance!=null){
+                return tempInstance
+            }
+
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
